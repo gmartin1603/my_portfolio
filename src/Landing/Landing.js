d8573ca280aa7b61aa5503323db603c2406d5c75
G Martin <georgemartin1603@yahoo.com>
2020-11-15 03:47:13 -0600

    Updated picture

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Landing.css'
 import Logo from '../Images/logo_transparent.png'
-import george from '../Images/me.jpg'
 
 
 function Landing(props) {
@@ -10,7 +9,7 @@ function Landing(props) {
             <div className="landing__tint">
                 <div className="landing__images">
                 <img src={Logo} alt="logo"/>
-                <img src={george} alt="George Martin"/>
+                <img src='https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/123789873_10223826723993416_1285154283843763912_n.jpg?_nc_cat=103&ccb=2&_nc_sid=09cbfe&_nc_ohc=UH8hmLWBlkgAX999WCU&_nc_ht=scontent-den4-1.xx&oh=1c91b0c4a54eea01eeeb227aba61d467&oe=5FD6DDB6' alt="George Martin"/>
                 </div>
                 <div className="landing__text">
                 <h2>Hi, I'm George Martin and I'm a Web Developer</h2>
